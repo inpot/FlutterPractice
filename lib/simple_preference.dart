218918d89228c8757f06e627ac3ec6ff4050ca5b
Daniel.Luo <winepot2005@gmail.com>
2020-03-25 10:17:53 +0800

    update windows files

--- lib/simple_preference.dart
+++ lib/simple_preference.dart
@@ -6,24 +6,31 @@ import 'package:path_provider/path_provider.dart';
 class SimplePref {
   Map<String, dynamic> content;
   File prefFile;
-  static var defaltEncoding = Encoding.getByName("utf-8");
+  static var _defaltEncoding = Encoding.getByName("utf-8");
+  static Map<String,SimplePref> _prefs = new Map();
 
   SimplePref(this.content, this.prefFile);
 
   static Future<SimplePref> getPref(String prefName) async {
+    var result = _prefs[prefName];
+    if(result != null){
+      return result;
+    }
     var prefDir = await getApplicationSupportDirectory();
-    var path = "${prefDir.path}${Platform.pathSeparator}$prefName";
+    var path = "${prefDir.path}${Platform.pathSeparator}$prefName.data";
     var file = File(path);
     Map<String, dynamic> content = Map();
     if (!await file.exists()) {
       await file.create(recursive: true);
     } else {
-      var contentStr = await file.readAsString(encoding: defaltEncoding);
+      var contentStr = await file.readAsString(encoding: _defaltEncoding);
       if (contentStr.isNotEmpty) {
         content = jsonDecode(contentStr);
       }
     }
-    return SimplePref(content, file);
+    result = SimplePref(content,file);
+    _prefs[prefName] = result;
+    return result;
   }
 
   void putInt(String key, int value) async {
